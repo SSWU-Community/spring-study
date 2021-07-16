6f473442c17d506dcd3c76c78a66254c4ce3bce1
chaeppy <chaeppy108@gmail.com>
2021-07-16 22:15:33 +0900

    [add] PostLike Entity

---
+++ src/main/java/sungshin/sooon/domain/entity/PostLike.java
@@ -0,0 +1,27 @@
+package sungshin.sooon.domain.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostLike {
+
+    @Id @GeneratedValue
+    @Column(name = "post_like_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
