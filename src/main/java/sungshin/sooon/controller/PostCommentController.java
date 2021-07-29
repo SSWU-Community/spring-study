524d498305e98ecaef6718c193f9637b3670c6a8
chaeppy <chaeppy108@gmail.com>
2021-07-29 19:40:26 +0900

    Feat: 추가 PostComment Api

--- src/main/java/sungshin/sooon/controller/PostCommentController.java
+++ src/main/java/sungshin/sooon/controller/PostCommentController.java
@@ -10,50 +10,54 @@ import sungshin.sooon.domain.entity.CurrentUser;
 import sungshin.sooon.domain.entity.Post;
 import sungshin.sooon.dto.CommentRequestDto;
 import sungshin.sooon.dto.CommentResponseDto;
-import sungshin.sooon.dto.PostResponseDto;
 import sungshin.sooon.service.PostCommentService;
 import sungshin.sooon.service.PostService;
-
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/posts/{commentId}/comments") //https://docs.spring.io/spring-framework/docs/3.0.0.M3/reference/html/ch18s02.html
+@RequestMapping("/api/v1/posts/{postId}/comments")
+//질문: findAllbyPost와 save, update를 제외하고는 comment의 id값은 post와는 독립적인데 이렇게 하위 리소스로 구분시켜도 되나?
+//그러니까 post의 1번 댓글 posts/{postId}/comments/1 이 아니라 포스트의 몇번째 댓글인지 상관없이 아이디값이 결정되므로 posts/{postId}/comments/412 이런식으로 되는데 괜찮은건가?
+//https://docs.spring.io/spring-framework/docs/3.0.0.M3/reference/html/ch18s02.html
 @Validated
 public class PostCommentController {
     private final PostCommentService postCommentService;
     private final PostService postService;
 
     @GetMapping("")
-    public ResponseEntity findAll() {
-        List<CommentResponseDto> postComments = postCommentService.findAll();
+    public ResponseEntity findAllByPost(@PathVariable long postId) {
+        Post post = postService.findByIdOrThrowNotFoundException(postId);
+        List<CommentResponseDto> postComments = postCommentService.findAllByPost(post);
         return new ResponseEntity(postComments, HttpStatus.OK);
     }
 
     @GetMapping("/{commentId}")
     public ResponseEntity findById(@PathVariable long postId, @PathVariable long commentId) {
-        Post post = postService.findByIdOrThrowNotFoundException(postId);
-        PostResponseDto post = postCommentService.findById(commentId);
-        return new ResponseEntity(post, HttpStatus.OK);
+        postService.findByIdOrThrowNotFoundException(postId);
+        return new ResponseEntity(postCommentService.findById(commentId), HttpStatus.OK);
     }
 
     @PostMapping("")
     public ResponseEntity save(@CurrentUser Account account, @PathVariable long postId, @Valid @RequestBody CommentRequestDto CommentRequestDto) {
+        Post post = postService.findByIdOrThrowNotFoundException(postId);
         return new ResponseEntity(postCommentService.save(account, post, CommentRequestDto), HttpStatus.CREATED);
     }
 
 
     @PutMapping("/{commentId}")
     public ResponseEntity update(@CurrentUser Account account, @PathVariable long postId, @PathVariable long commentId, @Valid @RequestBody CommentRequestDto CommentRequestDto) {
-        return new ResponseEntity(postCommentService.update(account, post, CommentRequestDto), HttpStatus.OK);
+        Post post = postService.findByIdOrThrowNotFoundException(postId);
+        return new ResponseEntity(postCommentService.update(account, post, commentId, CommentRequestDto), HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity delete(@CurrentUser Account account, @PathVariable long postId, @PathVariable long commentId) {
-        postCommentService.delete(account, postId);
+        postService.findByIdOrThrowNotFoundException(postId);
+        postCommentService.delete(account, commentId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/sungshin/sooon/service/PostCommentService.java
+++ src/main/java/sungshin/sooon/service/PostCommentService.java
@@ -9,7 +9,6 @@ import sungshin.sooon.domain.entity.Account;
 import sungshin.sooon.domain.entity.Post;
 import sungshin.sooon.domain.entity.PostComment;
 import sungshin.sooon.domain.repository.PostCommentRepository;
-import sungshin.sooon.domain.repository.PostRepository;
 import sungshin.sooon.dto.CommentRequestDto;
 import sungshin.sooon.dto.CommentResponseDto;
 import sungshin.sooon.exception.NotFoundException;
@@ -21,38 +20,70 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class PostCommentService {
-    private final PostRepository postRepository;
     private final PostCommentRepository postCommentRepository;
 
     @Transactional(readOnly = true)
-    public List<CommentResponseDto> findAll() {
+    public List<CommentResponseDto> findAllByPost(Post post) {
         return postCommentRepository
-                .findAll()
+                .findAllByPost(post)
                 .stream()
-                .map(CommentRequestDto::of)
+                .map(CommentResponseDto::of)
                 .collect(Collectors.toList());
     }
 
     @Transactional(readOnly = true)
-    public CommentResponseDto findById(Long postId, Long commentId) {
-        PostComment postComment = findByIdOrThrowNotFoundException(commentId);
-        return CommentRequestDto.of(postComment);
+    public CommentResponseDto findById(long id) {
+        PostComment postComment = findByIdOrThrowNotFoundException(id);
+        return CommentResponseDto.of(postComment);
     }
 
-
     @Transactional
-    public CommentResponseDto save(Account account, long postId ,CommentRequestDto commentRequestDto) {
+    public CommentResponseDto save(Account account, Post post, CommentRequestDto commentRequestDto) {
+        //익명이 아닌 경우
         PostComment postComment = commentRequestDto.toComment();
         postComment.setAccount(account);
-        postComment.setPost(account);
+        postComment.setPost(post);
+
+        //익명인 경우 orderNum 설정
+        if (commentRequestDto.isAnonymous()) {
+            postComment.setOrderNum(getCurrentPostCommentOrderNum(account, post));
+        }
+
         return CommentResponseDto.of(postCommentRepository.save(postComment));
     }
 
+    /*
+    NESTED
+    부모 트랜잭션이 존재한다면 중첩 트랜잭션을 생성합니다.
+    중첩된 트랜잭션 내부에서 롤백 발생시 해당 중첩 트랜잭션의 시작 지점 까지만 롤백됩니다.
+    중첩 트랜잭션은 부모 트랜잭션이 커밋될 때 같이 커밋됩니다.
+    부모 트랜잭션이 존재하지 않는다면 새로운 트랜잭션을 생성합니다.
+    출처:https://deveric.tistory.com/86
+     */
+    @Transactional(readOnly = true)
+    private long getCurrentPostCommentOrderNum(Account account, Post post) {
+        PostComment CommentWithMyOrderNum = postCommentRepository.findTop1ByAccountAndPostAndIsAnonymous(account, post, true);
+        if (CommentWithMyOrderNum != null) {
+            return CommentWithMyOrderNum.getOrderNum();
+        } else {
+            return getLastPostCommentOrderNum(post);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    private long getLastPostCommentOrderNum(Post post) {
+        PostComment CommentWithLastOrderNum = postCommentRepository.findTop1ByPostAndIsAnonymousOrderByCreatedAtDesc(post, true);
+        if (CommentWithLastOrderNum != null) {
+            return CommentWithLastOrderNum.getOrderNum() + 1;
+        } else {
+            return 1;
+        }
+    }
+
     @Transactional
-    public void delete(Account account, Long id) {
+    public void delete(Account account, long id) {
         PostComment postComment = findByIdOrThrowNotFoundException(id);
 
-        //현재사용자와 게시글을 작성한 유저의 아이디를 비교해서 권한이있는지 확인해야함. 인터셉터로 해야하나?? 서비스단 말고 더 좋은 위치는 없나?
         if (postComment.getAccount().getId() != account.getId()) {
             throw new AccessDeniedException("삭제 권한이 없습니다.");
         }
@@ -60,23 +91,27 @@ public class PostCommentService {
         postCommentRepository.delete(postComment);
     }
 
-    private PostComment findByIdOrThrowNotFoundException(Long id){
-        PostComment postComment = postCommentRepository
-                .findById(id)
-                .orElseThrow(() -> new NotFoundException("댓글이 존재하지 않습니다."));
-        return postComment;
-    }
-
     @Transactional
-    public CommentResponseDto update(Account account, Long id, CommentRequestDto commentRequestDto) {
+    public CommentResponseDto update(Account account, Post post, long id, CommentRequestDto commentRequestDto) {
         PostComment postComment = findByIdOrThrowNotFoundException(id);
 
-        //현재사용자와 게시글을 작성한 유저의 아이디를 비교해서 권한이있는지 확인해야함. 인터셉터로 해야하나?? 서비스단 말고 더 좋은 위치는 없나?
         if (postComment.getAccount().getId() != account.getId()) {
             throw new AccessDeniedException("수정 권한이 없습니다.");
         }
 
-        CommentRequestDto.apply(postComment);
-        return commentResponseDto.of(postComment);
+        //유저가 제일 처음 댓글을 닉네임으로 달고 그 후에 익명으로 수정하여 orderNum이 필요한 경우 고려
+        if (commentRequestDto.isAnonymous()) {
+            postComment.setOrderNum(getCurrentPostCommentOrderNum(account, post));
+        }
+
+        commentRequestDto.apply(postComment);
+        return CommentResponseDto.of(postComment);
+    }
+
+    private PostComment findByIdOrThrowNotFoundException(long id) {
+        PostComment postComment = postCommentRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("댓글이 존재하지 않습니다."));
+        return postComment;
     }
 }
